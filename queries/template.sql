8ce95d749f3dc5a4da27b739613b5a53b96f7d78
tyedem <90783116+tyedem@users.noreply.github.com>
2022-08-05 10:36:05 -0400

    Add queries folder with .sql files

---
+++ queries/template.sql
@@ -0,0 +1,41 @@
+/* Table Queries Template:
+
+Big Query SQL files follow this template structure to keep relevant queries organized and easy to find for the goal of the query type.
+
+*/
+
+-- 1) daily_activity
+
+-- 2) daily_calories
+
+-- 3) daily_intensities
+
+-- 4) daily_steps
+
+-- 5) heartrate_seconds
+
+-- 6) daily_steps
+
+-- 7) hourly_intensities
+
+-- 8) hourly_steps
+
+-- 9) minute_calories_narrow
+
+-- 10) minute_calories_wide
+
+-- 11) minute_calories_wide
+
+-- 12) minute_intensities_wide
+
+-- 13) minute_mets_narrow
+
+-- 14) minute_sleep
+
+-- 15) minute_steps_narrow
+
+-- 16) minute_steps_wide
+
+-- 17) sleep_day
+
+-- 18) weight_log_info
